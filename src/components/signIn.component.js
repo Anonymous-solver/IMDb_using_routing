63a3da4c7c27517ad831cf28a328e451c9ddff83
Anik Barua <anik.uits29@gmail.com>
2022-01-29 15:42:38 +0600

    modified signin

--- src/components/signIn.component.js
+++ src/components/signIn.component.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 const SignIn = () => {
 	const [lists, setLists] = useState([]);
 	const [inputs, setInputs] = useState({
+		signIn: false,
 		id: 0,
 		fullName: "",
 		email: "",
@@ -52,24 +53,26 @@ const SignIn = () => {
 					onSubmit={handleSubmit}
 					style={{ textAlign: "center", margin: "20px" }}
 				>
-					<input
-						style={{
-							height: "3rem",
-							width: "40rem",
-							paddingLeft: "10px",
-							marginTop: "5px",
-							borderRadius: "20px",
-							border: "1px solid gray",
-							outline: "none",
-						}}
-						type="text"
-						name="fullName"
-						id="fullName"
-						value={inputs.fullName}
-						onChange={handleChange}
-						placeholder="Enter your name"
-						required
-					/>
+					{!inputs.signIn && (
+						<input
+							style={{
+								height: "3rem",
+								width: "40rem",
+								paddingLeft: "10px",
+								marginTop: "5px",
+								borderRadius: "20px",
+								border: "1px solid gray",
+								outline: "none",
+							}}
+							type="text"
+							name="fullName"
+							id="fullName"
+							value={inputs.fullName}
+							onChange={handleChange}
+							placeholder="Enter your name"
+							required
+						/>
+					)}
 					<br />
 					<input
 						style={{
@@ -109,42 +112,81 @@ const SignIn = () => {
 						required
 					/>
 					<br />
-					<input
-						style={{
-							height: "3rem",
-							width: "40rem",
-							paddingLeft: "10px",
-							marginTop: "5px",
-							borderRadius: "20px",
-							border: "1px solid gray",
-							outline: "none",
-						}}
-						type="text"
-						name="confirmPassword"
-						id="confirmPassword"
-						value={inputs.confirmPassword}
-						onChange={handleChange}
-						placeholder="Confirm password"
-						required
-					/>
+					{!inputs.signIn && (
+						<input
+							style={{
+								height: "3rem",
+								width: "40rem",
+								paddingLeft: "10px",
+								marginTop: "5px",
+								borderRadius: "20px",
+								border: "1px solid gray",
+								outline: "none",
+							}}
+							type="text"
+							name="confirmPassword"
+							id="confirmPassword"
+							value={inputs.confirmPassword}
+							onChange={handleChange}
+							placeholder="Confirm password"
+							required
+						/>
+					)}
 					<br />
-					<input
-						className="btn btn-warning"
-						style={{
-							paddingLeft: "10px",
-							marginTop: "5px",
-							borderRadius: "10px",
-							border: "1px solid white",
-						}}
-						type="submit"
-						value="Create a New Account"
-					/>
+					{inputs.signIn ? (
+						<input
+							className="btn btn-warning"
+							style={{
+								paddingLeft: "10px",
+								marginTop: "5px",
+								borderRadius: "10px",
+								border: "1px solid white",
+							}}
+							type="submit"
+							value="Sign In"
+						/>
+					) : (
+						<input
+							className="btn btn-warning"
+							style={{
+								paddingLeft: "10px",
+								marginTop: "5px",
+								borderRadius: "10px",
+								border: "1px solid white",
+							}}
+							type="submit"
+							value="Create a New Account"
+						/>
+					)}
 				</form>
+				{inputs.signIn ? (
+					<div style={{ textAlign: "center", marginBottom: "20px" }}>
+						<button
+							onClick={() =>
+								setInputs({ ...inputs, signIn: false })
+							}
+							className="btn btn-success"
+						>
+							I have no account
+						</button>
+					</div>
+				) : (
+					<div style={{ textAlign: "center", marginBottom: "20px" }}>
+						<button
+							onClick={() =>
+								setInputs({ ...inputs, signIn: true })
+							}
+							className="btn btn-success"
+						>
+							Already have an account?
+						</button>
+					</div>
+				)}
 			</div>
 			<div>
 				{lists.map((list) => {
 					return (
-						<div style={{marginTop: '10px'}}>
+						<div style={{ marginTop: "10px" }}>
 							{list.fullName}
 							<br />
 							{list.email}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<BrowserRouter>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</BrowserRouter>,
+	document.getElementById("root")
 );
-
-
